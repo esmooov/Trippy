46d56fb9a60ff6048a787e79bcdb7b7025d23b07
Al Shaw <almshaw@gmail.com>
2010-12-05 11:19:43 -0500

    some deployment stuff

---
+++ misc/ec2.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+cd ~
+
+apt-get -y update
+apt-get -y install build-essential
+apt-get -y install automake
+apt-get -y install checkinstall
+
+apt-get -y install ruby
+apt-get -y install ruby1.8-dev
+apt-get -y install libzlib-ruby
+apt-get -y install libyaml-ruby
+apt-get -y install libdrb-ruby
+apt-get -y install liberb-ruby
+apt-get -y install irb
+apt-get -y install rdoc
+apt-get -y install zlib1g-dev
+apt-get -y install libopenssl-ruby
+apt-get -y install git-core
+apt-get -y install mysql-server
+apt-get -y install mysql-client
+apt-get -y install libmysql-ruby libmysqlclient-dev
+apt-get -y install sqlite3 libsqlite3-dev libsqlite3-ruby1.8 libdbd-sqlite3-ruby1.8
+apt-get -y install libssl-dev 
+apt-get -y install libcurl4-openssl-dev
+apt-get -y install unzip
+apt-get -y update
+
+curl -O http://production.cf.rubygems.org/rubygems/rubygems-1.3.7.tgz && \
+tar xzvf rubygems-1.3.7.tgz && \
+cd rubygems-1.3.7 && \
+sudo ruby setup.rb
+sudo ln -s /usr/bin/gem1.8 /usr/bin/gem
+
+sudo gem update --system
+sudo gem install sqlite3-ruby rails capistrano \
+passenger -v 3.0.0 sinatra haml rest_client readability \ 
+sanitize twitter-text crack curb typhoeus --no-ri --no-rdoc
+
+sudo passenger-install-nginx-module --auto --prefix=/opt/nginx --auto-download
+
+# create the server folders
+sudo mkdir -p /var/www/html/trippy
+sudo mkdir -p /var/www/html/trippy/shared
+sudo chown -R ubuntu /var/www/html/trippy/shared
+
+#create git repo folder
+sudo mkdir -p /var/www/git/trippy
+git clone git://github.com/esmooov/Trippy.git /var/www/git/trippy
+chown -R ubuntu /var/www/git/trippy
+
+# install nginx.conf
+# which will run the server on port 81
+cd /opt/nginx/conf && \
+curl -O https://gist.github.com/raw/5c8de640c887b22a9446/trippy-nginx.conf
+
+# install nginx restarter in init.d
+cd /etc/init.d && \
+curl https://gist.github.com/raw/8a8a243b8afd6fe56667/nginx -o nginx
+sudo chmod +x nginx
+
+sudo /etc/init.d/nginx stop
+sudo /etc/init.d/nginx start
