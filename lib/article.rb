9202fbc5a0268563f82149e38ea26033ccc338e7
Jeff Larson <thejefflarson@gmail.com>
2010-12-05 01:22:14 -0500

    fixed the no ref error

--- lib/article.rb
+++ lib/article.rb
@@ -1,16 +1,17 @@
 class Article
   WPM = 250
 
-  attr_accessor :readability_html, :title
+  attr_accessor :readability_html, :title, :id
 
   def initialize(response)
     @readability_html = Readability::Document.new(response.body).content rescue ""
     @clean_text       = Sanitize.clean(readability_html) rescue ""
     @title            = Nokogiri::HTML(response.body).search('title').text rescue ""
+    @id               = Digest::MD5.hexdigest(@clean_text)
   end
 
   def wc
-    clean_text.split(/ /).size
+    @clean_text.split(/ /).size
   end
 
   def read_time
